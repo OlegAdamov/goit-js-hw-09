9c7b8b8314f3fa6e953a7766cb9543b27477efb0
Oleg Adamov <olegadamov84@gmail.com>
2022-08-25 23:23:43 +0300

    start second task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const onStartBtn = document.querySelector('button[data-start]');
+const onStoptBtn = document.querySelector('button[data-stop]');
+const onBodyPage = document.querySelector('body');
+const COLOR_CHANGE_DELAY = 1000;
+
+  intervalID = null;
+isActive = false;
+  
+const onStartChangeColorPage = () => {
+        if (this.isActive) {
+    return;
+    }
+      this.isActive = true;
+  this.intervalID = setInterval(() => {
+
+      const onNewColor = `${getRandomHexColor()}`
+      onBodyPage.style.backgroundColor = onNewColor;
+  }, COLOR_CHANGE_DELAY)
+}
+
+
+const onStopChangeColorPage = () => {
+  clearInterval(this.intervalID);
+this.isActive = false;
+}
+
+onStartBtn.addEventListener('click', onStartChangeColorPage);
+onStoptBtn.addEventListener('click', onStopChangeColorPage)
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
