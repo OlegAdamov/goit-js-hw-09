ed61dd6b25b8bd7a5d3ebea04ab0bf9dbeeba1c8
OlegAdamov <103196379+OlegAdamov@users.noreply.github.com>
2022-08-27 21:24:17 +0000

    Deploying to gh-pages from @ OlegAdamov/goit-js-hw-09@0671fe87bfb93c1f0bf904802983cb7f3ce45799 🚀

--- 01-color-switcher.933a2687.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={},e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),c=document.querySelector("body");intervalID=null,isActive=!1;e.addEventListener("click",(function(){t.isActive||(t.isActive=!0,t.intervalID=setInterval((function(){var t="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)));c.style.backgroundColor=t}),1e3))})),n.addEventListener("click",(function(){clearInterval(t.intervalID),t.isActive=!1}))}();
-//# sourceMappingURL=01-color-switcher.933a2687.js.map
